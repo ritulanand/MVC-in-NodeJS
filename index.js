50b4391888d3c4cdbdd842fc3760e01a52066d6c
Your Name <you@example.com>
2024-11-30 00:59:49 +0530

    view added in mvc

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // const express = require('express');
 import express from 'express';
 import ProductController from './src/controllers/product.controller.js';
-
+import path from "path";
 // const ProductController = require('./src/controllers/product.controller.js');
 const server = express();
 
 // server.get("/", (req, res) => {
 //     return res.send('welcome to inventory app')
 // })
+
+
+//setup view engine
+server.set('view engine', 'ejs');
+server.set('views', path.join(path.resolve(), 'src','views'))
 
 // crerate a instance of productcontroller 
 const productcontroller = new ProductController();
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -7,7 +7,8 @@ export default class ProductController{
     getProducts(req, res){
         let products = ProductModel.get();
         console.log(products);
-        console.log(path.resolve())
-       return  res.sendFile(path.join(path.resolve(), 'src','views', 'products.html'));
+        res.render("products", {products : products});
+        // console.log(path.resolve())
+    //    return  res.sendFile(path.join(path.resolve(), 'src','views', 'products.html'));
     }
 }
